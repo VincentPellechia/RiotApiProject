3b5ec651d3a734f8c8e93ab7206ba55129570b63
Vincent Pellechia <vincent.pellechia01@gmail.com>
2023-09-06 11:05:58 -0400

    Add participants to database + formatting

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -1,88 +1,100 @@
-const getMostRecentMatches = require('../utils/getMostRecentMatches'); // Import your utility functions
-const getMatchData = require('../utils/getMatchData');
-const addMatchesToDatabase = require('../utils/addMatchesToDatabase');
+const getMostRecentMatches = require("../utils/getMostRecentMatches"); // Import your utility functions
+const getMatchData = require("../utils/getMatchData");
+const addMatchesToDatabase = require("../utils/addMatchesToDatabase");
+const addParticipantsToDatabase = require("../utils/addParticipantsToDatabase");
 
 //TODO rename getMatches and getMatchInfo to getMatchesFromAPI and getMatchInfoFromAPI respectfully
 
 const getMatches = async (req, res) => {
   try {
     let region;
-    if(req.body.region === 'euw1' || req.body.region === 'eun1') {region = 'EUROPE';}
+    if (req.body.region === "euw1" || req.body.region === "eun1") {
+      region = "EUROPE";
+    }
 
-    if(req.body.region === 'na1'){region = 'AMERICAS';}
+    if (req.body.region === "na1") {
+      region = "AMERICAS";
+    }
     const puuid = req.body.userId;
 
     const recentGames = await getMostRecentMatches(puuid, region);
 
     res.json({ message: recentGames });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
   }
 };
 
 const getMatchInfo = async (req, res) => {
-    try{
-        let region;
-        if(req.body.region === 'euw1' || req.body.region === 'eun1') {region = 'EUROPE';}
-    
-        if(req.body.region === 'na1'){region = 'AMERICAS';}
-  
-        const matchIds = req.body.matches;
-        const matchDataArray = [];
-
-      // Loop through each match ID and fetch match data
-      for (const matchId of matchIds) {
-        const matchData = await getMatchData(matchId, region);
-        matchDataArray.push(matchData);
-      }
-
-      await addMatchesToDatabase(matchDataArray);
-      //await addParticipantsToDatabase(matchDataArray); todo
-      //await addParticipantStatsToDatabase(matchDataArray); todo
-      res.json({ message: matchDataArray });
+  try {
+    let region;
+    if (req.body.region === "euw1" || req.body.region === "eun1") {
+      region = "EUROPE";
     }
-    catch(error){
-      console.error('Error:', error);
-      res.status(500).json({ error: 'An error occurred' });
-    }
-}
 
-//TODO
-const getMatchesFromDatabase = async (req,res) =>{
-  try{
-    pool.query(`
-    SELECT * from matches left join participants on participants.match_id = match.match_id
-    where participants.summoner_name = ${summonerName}
-    `, (err, result) => {
-      if (err) {
-        return console.error('Error executing query', err.stack);
-      }
-      console.log('Query result:', result.rows);
-    });
-  }
-  catch(error){
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    if (req.body.region === "na1") {
+      region = "AMERICAS";
+    }
+
+    const matchIds = req.body.matches;
+    const matchDataArray = [];
+
+    // Loop through each match ID and fetch match data
+    for (const matchId of matchIds) {
+      const matchData = await getMatchData(matchId, region);
+      matchDataArray.push(matchData);
+    }
+
+    await addMatchesToDatabase(matchDataArray);
+    await addParticipantsToDatabase(matchDataArray);
+
+    res.json({ message: matchDataArray });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
   }
 };
 
 //TODO
-const getMatchInfoFromDatabase = async (req,res) =>{
-  try{
-    pool.query(`
+const getMatchesFromDatabase = async (req, res) => {
+  try {
+    pool.query(
+      `
+    SELECT * from matches left join participants on participants.match_id = match.match_id
+    where participants.summoner_name = ${summonerName}
+    `,
+      (err, result) => {
+        if (err) {
+          return console.error("Error executing query", err.stack);
+        }
+        console.log("Query result:", result.rows);
+      }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+};
+
+//TODO
+const getMatchInfoFromDatabase = async (req, res) => {
+  try {
+    pool.query(
+      `
     SELECT * from participant_stats left join participants on participants.participant_id = participant_stats.participant_id
     where participants.match_id = ${matchId}
-    `, (err, result) => {
-      if (err) {
-        return console.error('Error executing query', err.stack);
+    `,
+      (err, result) => {
+        if (err) {
+          return console.error("Error executing query", err.stack);
+        }
+        console.log("Query result:", result.rows);
       }
-      console.log('Query result:', result.rows);
-    });
-  }
-  catch(error){
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
   }
 };
 
---
+++ server/utils/addParticipantsToDatabase.js
@@ -0,0 +1,91 @@
+const { pool } = require("../db");
+const addParticipantStatsToDatabase = require("../utils/addParticipantStatsToDatabase");
+
+const addParticipantsToDatabase = async (matchesData) => {
+  let client; // Declare the client outside the try-catch block
+
+  try {
+    client = await pool.connect(); // Acquire a connection from the pool
+    // Start a transaction
+    await client.query("BEGIN");
+
+    for (const match of matchesData) {
+      const matchId = match.metadata.matchId;
+      for (const participant of match.info.participants) {
+        // Destructure participant data
+        const {
+          summonerName,
+          summonerId,
+          teamId,
+          championId,
+          championName,
+          // Add other relevant participant data here
+        } = participant;
+
+        // SQL query to insert participant data into participants table
+        const queryText = `INSERT INTO participants (match_id, summoner_id, summoner_name, champion_name, champion_id, team_id) 
+                            VALUES ($1, $2, $3, $4, $5, $6)
+                            RETURNING participant_id`; // Return the participant_id
+
+        // Data to be inserted for participants
+        const values = [
+          matchId,
+          summonerId,
+          summonerName,
+          championName,
+          championId,
+          teamId,
+        ];
+
+        // Execute the insert query for each match
+        const result = await client.query(queryText, values);
+
+        // Get the returned participant_id
+        const participantId = result.rows[0].participant_id;
+
+        const statsData = {
+          championName: championName,
+          win: participant.win,
+          kills: participant.kills,
+          assists: participant.assists,
+          deaths: participant.deaths,
+          role: participant.role,
+          lane: participant.lane,
+          damageDealt: participant.totalDamageDealtToChampions,
+          damageTaken: participant.totalDamageTaken,
+          goldEarned: participant.goldEarned,
+          championLevel: participant.champLevel,
+          item0: participant.item0,
+          item1: participant.item1,
+          item2: participant.item2,
+          item3: participant.item3,
+          item4: participant.item4,
+          item5: participant.item5,
+          item6: participant.item6,
+          totalMinionsKilled: participant.totalMinionsKilled,
+          summoner1Id: participant.summoner1Id,
+          summoner2Id: participant.summoner2Id,
+          largestMultiKill: participant.largestMultiKill,
+        };
+
+        // Pass participantId to addParticipantStatsToDatabase along with relevant stats data
+        await addParticipantStatsToDatabase(client, participantId, statsData);
+      }
+    }
+    // Commit the transaction
+    await client.query("COMMIT");
+  } catch (error) {
+    // If there's an error, rollback the transaction
+    if (client) {
+      await client.query("ROLLBACK");
+    }
+    console.error("Error adding matches to the database:", error);
+    throw error;
+  } finally {
+    if (client) {
+      client.release(); // Release the connection back to the pool
+    }
+  }
+};
+
+module.exports = addParticipantsToDatabase;
