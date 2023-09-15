af3030b13ff1db53ce94365cfad7cf59e5099a08
Vincent Pellechia <vincent.pellechia01@gmail.com>
2023-09-15 14:38:05 -0400

    updating variables with data changes

--- client/src/components/matchList.js
+++ client/src/components/matchList.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Match from './match.js';
+import React, { useEffect } from "react";
+import Match from "./match.js";
 
 const MatchList = ({ userId, matches }) => {
-    if(matches < 0){
-        return (<div>Loading ...</div>)
-    }
+  if (matches < 0) {
+    return <div>Loading ...</div>;
+  }
   return (
     <div className="match-list">
-      {matches.map((match, index) => (
-        match ? (<Match key={index} userId = {userId} match={match} />
-        ) : null))
-        }
+      {matches.map((match, index) =>
+        match ? <Match key={index} userId={userId} match={match} /> : null
+      )}
     </div>
   );
 };
