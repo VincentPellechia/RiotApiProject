d0d4b9d1e73ba54149025c233d9c1664e4c2fb0d
Vincent Pellechia <vincent.pellechia01@gmail.com>
2023-09-26 15:55:50 -0400

    updated champion styling

--- client/src/components/champions.js
+++ client/src/components/champions.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import img from "../assets/temp.png";
 import * as userService from "../services/user.js"; // Import the userService
 import { formatAverage, getAverage } from "../utils/helpers.js";
+import "../styling/champions.css";
 
 const Champions = ({ user, region }) => {
   const [championData, setChampionData] = useState([]);
@@ -32,30 +33,39 @@ const Champions = ({ user, region }) => {
   }, [region, user]);
 
   return (
-    <div className="champions">
+    <div className="champions-container">
       {loadingChampions ? (
         <p>Loading Champions...</p>
       ) : (
-        <ul>
+        <ul className="champions-list">
           {championData.map((champion) => (
-            <li key={champion.champion_id}>
+            <li key={champion.champion_id} className="champions-item">
               <div>
                 <img
                   src={img}
                   alt={`Champion ${champion.champion_name}`}
                   width="30"
                   height="30"
+                  className="champions-image"
                 />
               </div>
-              <div>
-                <p>Champion: {champion.champion_name}</p>
-                <p>{getAverage(champion.wins, champion.losses)}%</p>
-                <p>Wins: {champion.wins}</p>
-                <p>Loses: {champion.losses}</p>
-                <p>Kills: {formatAverage(champion.average_kills)}</p>
-                <p>Deaths: {formatAverage(champion.average_deaths)}</p>
-                <p>Assists: {formatAverage(champion.average_assists)}</p>
-                <p>Creep Score: {formatAverage(champion.creep_score)}</p>
+              <div className="champions-details">
+                <div className="champion-name">{champion.champion_name}</div>
+                <div className="champion-games">
+                  <p>{getAverage(champion.wins, champion.losses)}%</p>
+                  <p>Wins: {champion.wins}</p>
+                  <p>Loses: {champion.losses}</p>
+                </div>
+                <div className="champion-kda">
+                  <p>
+                    {formatAverage(champion.average_kills) +
+                      "/" +
+                      formatAverage(champion.average_deaths) +
+                      "/" +
+                      formatAverage(champion.average_assists)}
+                  </p>
+                  <p>CS {formatAverage(champion.creep_score)}</p>
+                </div>
               </div>
             </li>
           ))}
