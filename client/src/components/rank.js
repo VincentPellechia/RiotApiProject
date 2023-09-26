17801acdb9a82db86efd202f5b72d5246c00c191
Vincent Pellechia <vincent.pellechia01@gmail.com>
2023-09-26 15:56:06 -0400

    updated rank styling and content

--- client/src/components/rank.js
+++ client/src/components/rank.js
@@ -1,35 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import * as userService from '../services/user.js';
+import React, { useEffect, useState } from "react";
+import * as userService from "../services/user.js";
+import img from "../assets/temp.png";
+import "../styling/rank.css";
+import { getAverage } from "../utils/helpers.js";
 
 const Rank = ({ summonerId, region }) => {
-    
-    const [rankData, setRankData] = useState(null);
+  const [rankData, setRankData] = useState(null);
 
-    useEffect(() => {
-        const fetchRank = async () => {
-            try {
-              const rankData = await userService.fetchRank(region, summonerId);
-              setRankData(rankData);
-            }
-            catch(error){
-                console.error('Error fetching rank data:', error);
-            }
-        }
-        fetchRank();
-
-    },[region,summonerId])
-
-    if (!rankData) {
-        return <p>Loading rank data...</p>;
+  useEffect(() => {
+    const fetchRank = async () => {
+      try {
+        const rankData = await userService.fetchRank(region, summonerId);
+        setRankData(rankData);
+      } catch (error) {
+        console.error("Error fetching rank data:", error);
       }
+    };
+    fetchRank();
+  }, [region, summonerId]);
 
+  if (!rankData) {
+    return <p>Loading rank data...</p>;
+  }
 
   return (
-    <div>
-      <p>Solo Rank: {rankData.tier} {rankData.rank} {rankData.leaguePoints} LP</p>
-      <p>Wins: {rankData.wins}</p>
-      <p>Losses: {rankData.losses}</p>
-      <p>Hot Streak: {rankData.hotStreak ? 'Yes' : 'No'}</p>
+    <div className="rank-container">
+      <div>
+        <img
+          src={img}
+          alt={`Champion ${rankData.tier}`}
+          width="100"
+          height="100"
+          className="rank-image"
+        />
+      </div>
+      <div className="rank-data">
+        <p>
+          Solo Rank: {rankData.tier} {rankData.rank} {rankData.leaguePoints} LP
+        </p>
+        <p>
+          {rankData.wins}W {rankData.losses}L
+        </p>
+        <p>{getAverage(rankData.wins, rankData.losses)}%</p>
+        <p>Hot Streak: {rankData.hotStreak ? "Yes" : "No"}</p>
+      </div>
     </div>
   );
 };
